0c9b1816c95b741a3c3bd3fe02fd8b6362184c66
Shahid Khan <shahidkhan@gmail.com>
2020-06-11 09:32:54 +0530

    author add and search

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const Author = require('../models/author')
 
 //All Authors Route
-router.get('/',(req,res) => {
-    res.render('authors/index');
+router.get('/',async (req,res) => {
+    let searchOptions = {};
+    if(req.query.name != null && req.query.name != '') {
+        searchOptions.name = new RegExp(req.query.name, 'i');
+    }
+    try {
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', { 
+             authors: authors,
+             searchOptions: req.query
+        })
+    } catch (error) {
+        console.log('error::',error);
+        res.redirect('/')
+    }
 })
 
 
@@ -15,8 +28,19 @@ router.get('/new', (req,res) => {
 })
 
 //Create Author Route
-router.post('/', (req,res) => {
-    res.render('Create');
+router.post('/', async (req,res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save();
+        res.redirect('authors');
+    } catch (error) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error Creating Author'
+        })
+    }
 })
 
 module.exports = router;
